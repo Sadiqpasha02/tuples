5f1404bd947583a30b090d923a2951d417f314be
Sadiqpasha02 <68812483+Sadiqpasha02@users.noreply.github.com>
2023-07-02 11:37:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+
+
+var = (12,2,45.6,890,8)
+print (var,type(var))
+print(var[2])
+#hello this is sadiq
+
+
+
+print(var[0:4])
+print(var[2:])
+print(var[0:3])
+print(var[:4:2])
+#
+print(var[-5:-1])
+print(var[-4::2])
+print(var[0:4:1])
+print(var[2:-1])
+print(var[-1:-5:-1])
+print(var[2:])
+
+#'''
+
+var1 = [1,2,34,5,6,7,7,8,9]
+var2 = [10,11,12,13,15,14,15,16,17]
+print ('var 1 = ',var1)
+print ('var 2 = ',var2)
+var1 = var1+var2
+print ('after concatenating = ',var1)
+
+# adding the tuples
+print (sum(var1))
+#
+print(max(var1))
+print (min(var1))
+#
+print(sorted (var1))
+#
+print(tuple(reversed(var1)))
+
+
+'''
+n = len(var)
+print('lengthof the tuple  is',var )
+i=0
+while i<n :
+  print(var[i])
+  i+=1
+print()
+
+for i in range(n):
+    print(var[i])
+
+print()
+
+for int  in var:
+    print(int)
+print ()
+for d in int in enumerate(var):
+        print(d,char,sep =':')
+
+'''
+
+
+
+
